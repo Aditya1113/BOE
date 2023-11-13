146d95cd6b963aec3e756e47a06181738f7de7cd
Aditya Mazumdar <adityamazumdar@Adityas-MacBook-Air.local>
2023-11-13 20:54:07 +0530

    api-consume

---
+++ ES6/API_consume.js
@@ -0,0 +1,37 @@
+let api = "https://jsonplaceholder.typicode.com/users";
+  
+//used to consume api
+let getRemoteData = (api) => {
+
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+      appendToHtml(data);
+    })
+    .catch((error) => console.log(error));
+};
+
+getRemoteData(api);
+
+
+//used to display data in a tabular format
+let appendToHtml = (data) => {
+  
+
+  for (let users of data) {
+    let tr = document.createElement("tr");
+    let td_1 = document.createElement("td");
+    let td_2 = document.createElement("td");
+    let td_3 = document.createElement("td");
+
+    td_1.innerHTML = `${users.name}`;
+    td_2.innerHTML = `${users.email}`;
+    td_3.innerHTML = `${users.username}`;
+
+    tr.appendChild(td_1);
+    tr.appendChild(td_2);
+    tr.appendChild(td_3);
+
+    document.getElementById("myTable").appendChild(tr);
+  }
+};
