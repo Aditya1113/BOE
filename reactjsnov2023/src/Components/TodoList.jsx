d5d93505ad9bd1ccb7f4db0af3bb129b6ed9a399
Aditya1113 <aditya26.mam@gmail.com>
2023-11-14 21:25:24 +0530

    FilterTodo

--- reactjsnov2023/src/Components/TodoList.jsx
+++ reactjsnov2023/src/Components/TodoList.jsx
@@ -7,7 +7,9 @@ const TodoList = () => {
     { title: "Meeting at 5", done: false },
   ]);
 
-  const [newTodo, setNewTodo] = useState("");
+  const [newTodo, setNewTodo] = useState({});
+
+  const [showCompleted, setShowCompleted] = useState(false);
 
   const toggleTodo = (todoToUpdate) => {
     //{title:'Walk the Dog',done:false},
@@ -23,7 +25,9 @@ const TodoList = () => {
   const AddTodo = () => {
     console.log("inside add");
 
-    const foundTodo = todoItems.find((item) => item.title.toLowerCase() === newTodo.toLowerCase());
+    const foundTodo = todoItems.find(
+      (item) => item.title.toLowerCase() === newTodo.toLowerCase()
+    );
 
     if (foundTodo) {
       alert("Oops Already There");
@@ -31,32 +35,34 @@ const TodoList = () => {
       setTodoItems([...todoItems, { title: newTodo, done: false }]);
     }
   };
-  const createTableBody = () => {
-    return todoItems.map((m, index) => (
-      <tr key={index}>
-        <td>{m.title}</td>
-        <td>
-          <p>
-            <label>
-              <input
-                type="checkbox"
-                onChange={() => toggleTodo(m)}
-                className="filled-in"
-                checked={m.done}
-              />
-              <span></span>
-            </label>
-          </p>
-        </td>
+  const createTableBody = (doneValue) => {
+    return todoItems
+      .filter((item) => item.done === doneValue)
+      .map((m, index) => (
+        <tr key={index}>
+          <td>{m.title}</td>
+          <td>
+            <p>
+              <label>
+                <input
+                  type="checkbox"
+                  onChange={() => toggleTodo(m)}
+                  className="filled-in"
+                  checked={m.done}
+                />
+                <span></span>
+              </label>
+            </p>
+          </td>
 
-        {/* <input type="checkbox" onChange={()=>toggleTodo(m)} checked='checked' className='filled-in'/> */}
-      </tr>
-    ));
+          {/* <input type="checkbox" onChange={()=>toggleTodo(m)} checked='checked' className='filled-in'/> */}
+        </tr>
+      ));
   };
   return (
     <div className="container s12">
       <div className="row">
-        <div className="input-field col s6">
+        <div className="input-field col s4">
           <input
             id="newTodo"
             type="text"
@@ -65,11 +71,10 @@ const TodoList = () => {
           />
           <label htmlFor="newTodo">Add new Todo</label>
         </div>
-        <div className="col s6">
-          <a className="waves-effect waves-light btn mt-3" onClick={AddTodo}>
-            Add
-          </a>
-        </div>
+
+        <button className="waves-effect waves-light btn m" onClick={AddTodo}>
+          Add
+        </button>
       </div>
       <table border={1} className="striped">
         <thead>
@@ -78,8 +83,26 @@ const TodoList = () => {
             <th>Done</th>
           </tr>
         </thead>
-        <tbody>{createTableBody()}</tbody>
+        <tbody>{createTableBody(false)}</tbody>
       </table>
+      <button
+        onClick={() => setShowCompleted(!showCompleted)}
+        className="waves-effect waves-light btn red"
+      >
+        Show Completed Tasks
+      </button>
+
+      {showCompleted ? (
+        <table border={1} className="striped">
+          <thead>
+            <tr>
+              <th>Todo Items</th>
+              <th>Done</th>
+            </tr>
+          </thead>
+          <tbody>{createTableBody(true)}</tbody>
+        </table>
+      ) :null}
     </div>
   );
 };
